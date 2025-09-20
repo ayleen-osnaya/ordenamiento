04bec35381c8919e6cbe424691dc38f25b42b4e2
ayleen <A01426008@tec.mx>
2025-09-20 12:24:40 -0600

    actualizacion

---
+++ cmake-build-debug/CMakeFiles/ordenamiento.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ordenamiento.dir/main.cpp.obj"
+  "CMakeFiles/ordenamiento.dir/main.cpp.obj.d"
+  "libordenamiento.dll.a"
+  "ordenamiento.exe"
+  "ordenamiento.exe.manifest"
+  "ordenamiento.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ordenamiento.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 using namespace std;
-
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+using namespace std;
 
 
