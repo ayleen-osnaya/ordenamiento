f1d9868389798db6c18cc461a43b26ca22b9f369
ayleen <A01426008@tec.mx>
2025-09-20 14:39:48 -0600

    actualizacion

--- main.cpp
+++ main.cpp
@@ -22,18 +22,18 @@ vector<string> cargarVector(string nombreArchivo) {
 }
 
 int numMes(string mes) {
-    if (mes == "Jan" || mes == "Ene") return 1;
+    if (mes == "Jan") return 1;
     if (mes == "Feb") return 2;
     if (mes == "Mar") return 3;
-    if (mes == "Apr" || mes == "Abr") return 4;
+    if (mes == "Apr") return 4;
     if (mes == "May") return 5;
     if (mes == "Jun") return 6;
     if (mes == "Jul") return 7;
-    if (mes == "Aug" || mes == "Ago") return 8;
+    if (mes == "Aug") return 8;
     if (mes == "Sep") return 9;
     if (mes == "Oct") return 10;
     if (mes == "Nov") return 11;
-    if (mes == "Dec" || mes == "Dic") return 12;
+    if (mes == "Dec") return 12;
     return 0;
 }
 
